45223d0c1b304491e2b8ddd8b51fbe47973f680a
Prince Karim <prikar79@gmail.com>
2022-11-11 09:48:42 -0500

    very minor changes | to seed file, recipes model, css

--- server/db/models/recipes.js
+++ server/db/models/recipes.js
@@ -10,15 +10,36 @@ const Recipes = db.define("recipe", {
     type: Sequelize.INTEGER,
     allowNull: false,
   },
+  cookTime: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  diet: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
   protein: {
     type: Sequelize.INTEGER,
+  },
+  carbs: {
+    type: Sequelize.INTEGER,
+  },
+  fat: {
+    type: Sequelize.INTEGER,
+  },
+  courseType: {
+    type: Sequelize.STRING,
   },
   cuisine: {
     type: Sequelize.STRING,
   },
-  diet: {
+  imageUrl: {
     type: Sequelize.STRING,
+    defaultValue:
+      "https://www.pngkey.com/png/detail/233-2332677_image-500580-placeholder-transparent.png",
   },
 });
 
 module.exports = Recipes;
+
+// https://toppng.com/uploads/preview/clipart-free-seaweed-clipart-draw-food-placeholder-11562968708qhzooxrjly.png
