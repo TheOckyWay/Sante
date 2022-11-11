590282c7a9a1b6c7d403e68e6425b51f81af42a2
Prince Karim <prikar79@gmail.com>
2022-11-11 12:31:22 -0500

    followed some pwa tutorial just going to check some deployment stuff cause why not

---
+++ public/serviceworker.js
@@ -0,0 +1,49 @@
+// CACHE_NAME is the storage of our browser
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
+
+// "self" refers to the service worker
+const self = this;
+
+// install service worker
+self.addEventListener("install", (event) => {
+  // open the cache and add to our urlsToCache to it
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// listen for requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    // storing all of our pages requests
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() =>
+        // this means there is no internet connection
+        caches.match("offline.html")
+      );
+    })
+  );
+});
+
+// activate the service worker
+self.addEventListener("activate", (event) => {
+  // remove all previous caches and just keep the new ones
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME);
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhiteList.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
