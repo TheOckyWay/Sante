9360dd0664244a606aae672a933ff1311c5e0f36
Andrew Ozoria <andrewozo123@gmail.com>
2022-11-10 19:57:36 -0500

    made us users

--- script/seed.js
+++ script/seed.js
@@ -5,15 +5,12 @@ const {
   models: { User, Recipes },
 } = require("../server/db");
 
-/**
- * seed - this function clears the database, updates tables to
- *      match the models, and populates the database.
- */
+
 async function seed() {
-  await db.sync({ force: true }); // clears db and matches models to tables
+  await db.sync({ force: true }); 
   console.log("db synced!");
 
-  // Creating Users
+ 
   const users = await Promise.all([
     User.create({ username: "Hamza", password: "123" }),
     User.create({ username: "Prince", password: "123" }),
@@ -55,11 +52,7 @@ async function seed() {
   };
 }
 
-/*
- We've separated the `seed` function from the `runSeed` function.
- This way we can isolate the error handling and exit trapping.
- The `seed` function is concerned only with modifying the database.
-*/
+
 async function runSeed() {
   console.log("seeding...");
   try {
@@ -74,14 +67,10 @@ async function runSeed() {
   }
 }
 
-/*
-  Execute the `seed` function, IF we ran this module directly (`node seed`).
-  `Async` functions always return a promise, so we can use `catch` to handle
-  any errors that might occur inside of `seed`.
-*/
+
 if (module === require.main) {
   runSeed();
 }
 
-// we export the seed function for testing purposes (see `./seed.spec.js`)
+
 module.exports = seed;
