6cbbbd5adc8a36f9dfb293518366cb4e28d6072e
Alvin Espinal <alvinjespinal@gmail.com>
2022-11-21 01:55:17 -0500

    finished first sign up page - basic info

--- client/components/auth/signup/Stepper.js
+++ client/components/auth/signup/Stepper.js
@@ -7,6 +7,8 @@ import {
 	Button,
 	Typography,
 } from '@mui/material';
+import BasicInfo from './BasicInfo';
+import WeightInfo from './WeightInfo';
 
 function StepperPage() {
 	const [activeStep, setActiveStep] = useState(0);
@@ -28,34 +30,40 @@ function StepperPage() {
 	];
 
 	return (
-		<Box sx={{ width: '100%' }}>
-			<Stepper activeStep={activeStep}>
-				{steps.map((label, index) => {
-					const stepProps = {};
-					const labelProps = {};
+		<>
+			<Box sx={{ width: '100%' }}>
+				<Stepper activeStep={activeStep}>
+					{steps.map((label, index) => {
+						const stepProps = {};
+						const labelProps = {};
 
-					return (
-						<Step key={label} {...stepProps}>
-							<StepLabel {...labelProps}>{label}</StepLabel>
-						</Step>
-					);
-				})}
-			</Stepper>
-			<Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-				<Button
-					color="inherit"
-					disabled={activeStep === 0}
-					onClick={handleBack}
-					sx={{ mr: 1 }}
-				>
-					Back
-				</Button>
-				<Box sx={{ flex: '1 1 auto' }} />
-				<Button onClick={handleNext}>
-					{activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-				</Button>
+						return (
+							<Step key={label} {...stepProps}>
+								<StepLabel {...labelProps}>{label}</StepLabel>
+							</Step>
+						);
+					})}
+				</Stepper>
+				<Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+					<Button
+						color="inherit"
+						disabled={activeStep === 0}
+						onClick={handleBack}
+						sx={{ mr: 1 }}
+					>
+						Back
+					</Button>
+					<Box sx={{ flex: '1 1 auto' }} />
+					<Button onClick={handleNext}>
+						{activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+					</Button>
+				</Box>
 			</Box>
-		</Box>
+			<div>
+				{activeStep === 0 ? <BasicInfo /> : ''}
+				{activeStep === 1 ? <WeightInfo /> : ''}
+			</div>
+		</>
 	);
 }
 
---
+++ client/components/auth/signup/WeightInfo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WeightInfo() {
+  return (
+	<div>WeightInfo</div>
+  )
+}
+
+export default WeightInfo
