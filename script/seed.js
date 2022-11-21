9a6330f315921eed7060bfe5fd64ddf74356866d
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2022-11-21 15:22:29 -0500

    bottom navbar working for loggin users

--- script/seed.js
+++ script/seed.js
@@ -11,9 +11,9 @@ async function seed() {
 
   const users = await Promise.all([
     User.create({ username: "Hamza", password: "123", firstName: 'Muhammad', lastName: 'Hamza', location: 'Queens, NY', email: 'hamza@foo.com', age: 25, sex: 'male', currentHeight: 60, currentWeight: 250, targetWeight: 200, startingWeight: 250, targetWater: 4000, activityFactor: 'Lightly Active',targetChange:'Lose Weight',}),
-    User.create({ username: "Prince", password: "123", firstName: 'Prince', lastName: 'Karim', location: 'Queens, NY', email: 'prince@fullstack.com'}),
-    User.create({ username: "Alvin", password: "123", firstName: 'Alvin', lastName: 'Espinal', location: 'New York, NY', email: 'alvin@fs.com'}),
-    User.create({ username: "Andrew", password: "123", firstName: 'Andrew', lastName: 'Espinal', location: 'Queens, NY', email: 'andrew@fs.com'}),
+    User.create({ username: "Prince", password: "123", firstName: 'Prince', lastName: 'Karim', location: 'Queens, NY', email: 'prince@fullstack.com', age: 25, sex: 'male', currentHeight: 60, currentWeight: 250, targetWeight: 200, startingWeight: 250, targetWater: 4000, activityFactor: 'Lightly Active',targetChange:'Lose Weight',}),
+    User.create({ username: "Alvin", password: "123", firstName: 'Alvin', lastName: 'Espinal', location: 'New York, NY', email: 'alvin@fs.com', age: 25, sex: 'male', currentHeight: 60, currentWeight: 250, targetWeight: 200, startingWeight: 250, targetWater: 4000, activityFactor: 'Lightly Active',targetChange:'Lose Weight',}),
+    User.create({ username: "Andrew", password: "123", firstName: 'Andrew', lastName: 'Espinal', location: 'Queens, NY', email: 'andrew@fs.com', age: 25, sex: 'male', currentHeight: 60, currentWeight: 250, targetWeight: 200, startingWeight: 250, targetWater: 4000, activityFactor: 'Lightly Active',targetChange:'Lose Weight',}),
   ]);
 
   // const trackers = await Promise.all([
@@ -162,8 +162,10 @@ async function seed() {
   console.log(`seeded successfully`);
   return {
     users: {
-      cody: users[0],
-      murphy: users[1],
+      Hamza: users[0],
+      Andrew: users[1],
+      Prince: users[2],
+      Alvin: users[3],
     },
   };
 }
