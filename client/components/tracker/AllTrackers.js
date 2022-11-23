c0b7f3c05b2ee334e1d7a93e226520084c5d5bd7
Prince Karim <prikar79@gmail.com>
2022-11-22 21:34:51 -0500

    did some styling | add water form api should work little better

--- client/components/tracker/AllTrackers.js
+++ client/components/tracker/AllTrackers.js
@@ -3,33 +3,46 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchTrackers } from "./trackerSlice";
 import SingleTracker from "./SingleTracker";
+import {
+  Container,
+  Typography,
+  Grid,
+  Card,
+  Button,
+  Box,
+  Stack,
+} from "@mui/material";
 
 function AllTracker() {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const trackers = useSelector((state) => state.tracker.allTracker);
+  const trackers = useSelector((state) => state.tracker.allTracker);
 
-	useEffect(() => {
-		dispatch(fetchTrackers());
-	}, []);
+  useEffect(() => {
+    dispatch(fetchTrackers());
+  }, []);
 
-	return (
-		<div>
-			<h1>Trackers</h1>
-			{trackers.map((tracker) => {
-				return (
-					<div key={tracker.id}>
-						<div>
-							<Link to={`/trackers/${tracker.id}`}>
-								<h2>{`${tracker.date}`}</h2>
-							</Link>
-							<h3>totalCalories: {tracker.totalCalories}</h3>
-						</div>
-					</div>
-				);
-			})}
-		</div>
-	);
+  return (
+    <Container>
+      <Typography variant="h4" textAlign="center">
+        Trackers
+      </Typography>
+      <Stack textAlign="center">
+        {trackers.map((tracker) => {
+          return (
+            <Card key={tracker.id} variant="outlined" sx={{ m: 2 }}>
+              <Link to={`/trackers/${tracker.id}`}>
+                <h2>{`${tracker.date}`}</h2>
+              </Link>
+              <Typography variant="h5">
+                Total Daily Calories: {tracker.totalCalories}
+              </Typography>
+            </Card>
+          );
+        })}
+      </Stack>
+    </Container>
+  );
 }
 
 export default AllTracker;
--- server/db/db.js
+++ server/db/db.js
@@ -26,4 +26,7 @@ const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   config
 );
+
+// const db = new Sequelize(`postgres://localhost:5432/${databaseName}`, config);
+
 module.exports = db;
