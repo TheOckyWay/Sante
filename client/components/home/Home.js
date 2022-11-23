e848ca9817d2410a932eb9ea792e1db871648ab6
Andrew Ozoria <andrewozo123@gmail.com>
2022-11-23 16:17:07 -0500

    added some simple styling

--- client/components/home/Home.js
+++ client/components/home/Home.js
@@ -1,72 +1,87 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchSingleTracker,fetchTrackers } from "../tracker/trackerSlice";
-import Avatar from '@mui/material/Avatar';
-import { Stack, Typography } from '@mui/material';
-import { blue } from '@mui/material/colors';
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
+import { fetchSingleTracker, fetchTrackers } from "../tracker/trackerSlice";
+import Avatar from "@mui/material/Avatar";
+import { Stack, Typography } from "@mui/material";
+import { blue } from "@mui/material/colors";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 /**
  * COMPONENT
  */
 const Home = (props) => {
   const dispatch = useDispatch();
-  
- 
-  
+
   const user = useSelector((state) => state.auth.me);
   const trackers = useSelector((state) => state.tracker.allTracker);
-  let tracker = trackers[trackers.length-1]
+  let tracker = trackers[trackers.length - 1];
 
-  useEffect(() =>{
-    dispatch(fetchTrackers())
-   }, []);
+  useEffect(() => {
+    dispatch(fetchTrackers());
+  }, []);
 
-    
-if (trackers.length && tracker){
-  const {totalCalories, waterIntake, totalCarbs, totalProtein, totalFat, date} = tracker
-  return (
-    <div>
-    <Stack direction="column"
-		justifycontent="space-evenly"
-		alignitems="center"
-    spacing={3}
-    >
+  if (trackers.length && tracker) {
+    const {
+      totalCalories,
+      waterIntake,
+      totalCarbs,
+      totalProtein,
+      totalFat,
+      date,
+    } = tracker;
+    return (
+      <div>
+        <Stack
+          direction="column"
+          justifycontent="space-evenly"
+          alignitems="center"
+          spacing={3}
+        >
+          <Stack
+            direction="row"
+            justifycontent="space-evenly"
+            alignitems="center"
+            spacing={1}
+          >
+            <Avatar sx={{ bgcolor: "#4361ee" }}>
+              {user.firstName[0]}
+              {user.lastName[0]}
+            </Avatar>
+            <Typography variant="h4" color="#000">
+              Welcome {user.username}!
+            </Typography>
+          </Stack>
 
+          <Typography float="left" variant="h5" color="#000">
+            {" "}
+            Calories for Today({date}):
+          </Typography>
 
-    <Stack
-		direction="row"
-		justifycontent="space-evenly"
-		alignitems="center"
-		spacing={1}
-		>
-    <Avatar sx={{ bgcolor: blue[900] }}>{user.firstName[0]}{user.lastName[0]}</Avatar>
-		<Typography variant="h4">Welcome {user.username}!</Typography>
-    </Stack>
+          <Stack direction="row" border="1px solid #f5f5f5" width="100%">
+            <div
+              direction="column"
+              justifycontent="flex-end"
+              alignitems="flex-end"
+            >
+              <Typography color="#000">Calories: {totalCalories}</Typography>
+              <Typography color="#000">Protein:{totalProtein} </Typography>
+              <Typography color="#000">Carbs:{totalCarbs} </Typography>
+              <Typography color="#000">Fats:{totalFat} </Typography>
+            </div>
+          </Stack>
+          <Stack direction="row" border="1px solid #f5f5f5" width="100%">
+            <div
+              direction="column"
+              justifycontent="flex-end"
+              alignitems="flex-end"
+            >
+              <Typography color="#000">Water: {waterIntake}</Typography>
+            </div>
+          </Stack>
+        </Stack>
 
-    <Typography float= 'left' variant="h5"> Calories for Today({date}):</Typography>
-    
-    
-    <Stack direction="row" border= '1px solid grey' width='100%'>
-          <div direction='column' justifycontent='flex-end' alignitems='flex-end'>
-          <Typography>Calories: {totalCalories}</Typography>
-          <Typography>Protein:{totalProtein} </Typography>
-          <Typography>Carbs:{totalCarbs} </Typography>
-          <Typography>Fats:{totalFat} </Typography>
-          </div>
-    </Stack>
-    <Stack direction="row" border= '1px solid grey' width='100%'>
-          <div direction='column' justifycontent='flex-end' alignitems='flex-end'>
-          <Typography>Water: {waterIntake}</Typography>
-          </div>
-    </Stack>
-
-
-    </Stack>
-
-
-      {/* <h1>{new Date}</h1>
+        {/* <h1>{new Date}</h1>
       {tracker !== undefined ? (
         <div>
           <p>Total Calories: {tracker.totalCalories}</p>
@@ -81,11 +96,11 @@ if (trackers.length && tracker){
           <p>No information to show at the moment</p>
         </div>
       )} */}
-    </div>
-  );
-  }else{
+      </div>
+    );
+  } else {
     return (
-      <Box sx={{ display: 'flex' }}>
+      <Box sx={{ display: "flex" }}>
         <CircularProgress color="success" />
       </Box>
     );
